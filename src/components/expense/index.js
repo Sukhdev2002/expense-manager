690bb0f393e05b4dc4ea22465561d981bef11a7e
Sukh Dev Suthar <2002sukhdevsuthar@gmail.com>
2024-06-23 00:41:13 +0530

    june-update

--- src/components/expense/index.js
+++ src/components/expense/index.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { jwtDecode as jwt_decode } from 'jwt-decode';
-import { Modal, Button, Row, Col, Table, Space } from 'antd';
+import { Modal, Button, Row, Col, Table, Space, Spin, Select } from 'antd';
 import MyForm from '../form/Form';
 import { postData, fetchData, deleteData, updateData } from '../../services/http-service';
+import { PieChart, Pie, Tooltip, Legend, ResponsiveContainer,Cell } from 'recharts';
 import './index.css'; // Import the CSS file for styles
 import _ from 'lodash';
+import moment from 'moment';
+
+const { Option } = Select;
 
 const Expense = () => {
   const [categories, setCategories] = useState([]);
@@ -15,8 +19,10 @@ const Expense = () => {
   const [categoryModalVisible, setCategoryModalVisible] = useState(false);
   const [expenseModalVisible, setExpenseModalVisible] = useState(false);
   const [filteredSubcategories, setFilteredSubcategories] = useState([]);
-
- 
+  const [chartData, setChartData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedMonth, setSelectedMonth] = useState(moment().startOf('month').format('YYYY-MM'));
+  const [totalMonthlyExpense, setTotalMonthlyExpense] = useState(0);
   const fetchCategories = async () => {
     try {
       const response = await fetchData(`/api/category`);
@@ -40,9 +46,10 @@ const Expense = () => {
     }
   };
 
-  const fetchExpenses = async () => {
+  const fetchExpenses = async (month) => {
     try {
-      const response = await fetchData(`/api/expenses`);
+      setLoading(true);
+      const response = await fetchData(`/api/expenses?month=${month}`);
       const data = await response.data;
 
       const updatedData = data.map((item) => ({
@@ -53,8 +60,27 @@ const Expense = () => {
         subcategory: _.get(item,['subcategory','name'], "N/A")
       }));
       setExpenses(updatedData);
+      setLoading(false);
     } catch (error) {
       console.error('Error fetching expenses:', error);
+      setLoading(false);
+    }
+  };
+
+  const fetchChartData = async (month) => {
+    try {
+      const response = await fetchData(`/api/expenses?month=${month}`);
+      const data = await response.data;
+    
+      const formattedData = data.map((item) => ({
+        name: _.get(item,['category','name'], "N/A"),
+        value: item.amount,
+      }));
+      let totalAmount = formattedData.reduce((acc, item) => acc + item.value, 0);
+      setTotalMonthlyExpense(totalAmount);
+      setChartData(formattedData);
+    } catch (error) {
+      console.error('Error fetching chart data:', error);
     }
   };
 
@@ -94,7 +120,7 @@ const Expense = () => {
       }));
       if (response.ok) {
         console.log('Expense added successfully');
-        fetchExpenses();
+        fetchExpenses(selectedMonth);
         setExpenseModalVisible(false);
       } else {
         console.error('Failed to add expense:', response.statusText);
@@ -104,13 +130,12 @@ const Expense = () => {
     }
   };
 
-
   const handleDeleteExpense = async (expenseId) => {
     try {
       const response = await deleteData(`/api/expenses/${expenseId}`);
       if (response.status === 200) {
         console.log('Expense deleted successfully');
-        fetchExpenses();
+        fetchExpenses(selectedMonth);
       } else {
         console.error('Failed to delete expense:', response.statusText);
       }
@@ -121,9 +146,25 @@ const Expense = () => {
 
   useEffect(() => {
     fetchCategories();
-    fetchExpenses();
- }, []);
+    fetchExpenses(selectedMonth);
+    fetchChartData(selectedMonth);
+  }, []);
 
+  useEffect(() => {
+    if (selectedMonth) {
+      fetchExpenses(selectedMonth);
+      fetchChartData(selectedMonth);
+    }
+  }, [selectedMonth]);
+
+  useEffect(() => {
+    if (selectedCategory) {
+      let categoryObj = JSON.parse(selectedCategory);
+      setFilteredSubcategories(subcategories.filter(sub => sub.categoryId === categoryObj.id));
+    } else {
+      setFilteredSubcategories([]);
+    }
+  }, [selectedCategory, subcategories]);
 
   const columns = [
     {
@@ -151,22 +192,11 @@ const Expense = () => {
       key: 'actions',
       render: (text, record) => (
         <Space size="middle">
-        
-        <Button onClick={() => handleDeleteExpense(record.id)} type="danger">Delete</Button>
+          <Button onClick={() => handleDeleteExpense(record.id)} type="danger">Delete</Button>
         </Space>
       ),
     },
   ];
-
-  useEffect(() => {
-    
-    if (selectedCategory) {
-      let categoryObj = JSON.parse(selectedCategory);
-      setFilteredSubcategories(subcategories.filter(sub => sub.categoryId === categoryObj.id));
-    } else {
-      setFilteredSubcategories([]);
-    }
-  }, [selectedCategory, subcategories]);
 
   const formConfig = [
     {
@@ -198,6 +228,12 @@ const Expense = () => {
     { type: 'text', label: 'Subcategories', name: 'subcategories', required: true, message: 'Please enter Subcategories!' }
   ];
 
+  const lastFiveMonths = [];
+  for (let i = 0; i < 5; i++) {
+    lastFiveMonths.push(moment().subtract(i, 'months').startOf('month').format('YYYY-MM'));
+  }
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AF19FF', '#00BFFF', '#FF00FF', '#FFFF00', '#808000', '#800000'];
+
   return (
     <div className="expense-container">
       <Row gutter={[16, 16]}>
@@ -226,6 +262,45 @@ const Expense = () => {
       >
         <MyForm fields={categoryFormConfig} onSubmit={handleAddCategory} />
       </Modal>
+
+      <div className="chart-container">
+        {/* <h2 className="expense-header">Expense Chart</h2> */}
+        <h1>Total Expense of Month { totalMonthlyExpense}</h1>
+        <Select
+          defaultValue={selectedMonth}
+          style={{ width: 200, marginBottom: 20 }}
+          onChange={(value) => setSelectedMonth(value)}
+        >
+          {lastFiveMonths.map(month => (
+            <Option key={month} value={month}>{month}</Option>
+          ))}
+        </Select>
+        {loading ? (
+          <Spin size="large" />
+        ) : (
+          <ResponsiveContainer width="100%" height={400}>
+            <PieChart>
+            <Pie
+                data={chartData}
+                dataKey="value"
+                nameKey="name"
+                cx="50%"
+                cy="50%"
+                outerRadius={150}
+                label
+                >
+                {
+                  chartData.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                  ))
+                }
+              </Pie>
+              <Tooltip />
+              <Legend />
+            </PieChart>
+          </ResponsiveContainer>
+        )}
+      </div>
 
       <div className="table-container">
         <h2 className="expense-header">Expense List</h2>
